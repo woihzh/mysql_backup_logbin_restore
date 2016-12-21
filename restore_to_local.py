213ccd13b61ec35ef0b5facf4b160d5a85ca96e7
Christopher <zwg_zero1@163.com>
2016-12-21 16:23:23 +0800

    save commit

--- restore_to_local.py
+++ restore_to_local.py
@@ -70,10 +70,50 @@ def send_mail(server=mail_server, user=mail_username, password=mail_password,
     envelope.add_attachment(attach_file)
     envelope.send(server, login=user, password=password, tls=True)
 
-def get_last_sql_file(fold, regular_m):
+def get_last_sql_file(fold, regular_m=backup_sql_prefix):
     files = os.listdir(fold)
     last_file = ''
     for item in files:
-        if re.match(r'^%s[\s\S]*?\.sql\.gz' % regular_m, item) and item > last_file
+        if re.match(r'^%s[\s\S]*?\.sql\.gz$' % regular_m, item) and item > last_file:
+            last_file = item
+    return last_file
+
+def get_bin_logs(fold, sql_name, regular_m=backup_binlog_prefix):
+    re_result = re.match('^[\s\S]*?_(%s\.\d+)_pos-(\d+)\.sql\.gz$' % regular_m, sql_name)
+    start_logbin_name = ""
+    pos = ""
+    if re_result:
+        start_logbin_name = re_result.group(1)
+        pos = re_result.group(2)
+    else:
+        print("can't find recorded binlog in sql backup file name")
+        logging.error("can't find recorded binlog in sql backup file name")
+
+    files = os.listdir(fold)
+    bin_logs = []
+    for item in files:
+        if re.match('^%s\.\d+$' % regular_m, item) and item >= start_logbin_name:
+            bin_logs.append(item)
+    return bin_logs, pos
+
 if __name__ == '__main__':
+    last_backup = get_last_sql_file(backup_fold)
+    if not last_backup:
+        print("Can't find backup sql file in %s" % backup_fold)
+        logging.error("Can't find backup sql file in %s" % backup_fold)
+        send_mail()
+        exit()
+    print('last backup sql file is: %s' % last_backup)
+    logging.info('last backup sql file is: %s' % last_backup)
+    binlogs, pos = get_bin_logs(backup_fold, last_backup)
+    if not binlogs or not pos:
+        print("No bin log files under %s" % backup_fold)
+        logging.error("No bin log files under %s" % backup_fold)
+        send_mail()
+        exit()
+    binlogs.sort()
+    binlog_str = ' '.join(binlogs)
+
+    # scp file to remote file
+    cmd = 'cd %s; scp -p %s %s %s@%s:%s/' % (backup_fold, binlog_str, last_backup, )
     cmd = 'zcat |sed '18,31d'|grep -av "SQL SECURITY DEFINER"'
